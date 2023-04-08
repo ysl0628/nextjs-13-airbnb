ef6d44c6139aa10f566e2912b32bfcef67862cb6
ysl0628 <yihsinlan@gmail.com>
2023-04-08 20:36:28 +0800

    feat(login): create action to get current user by next-auth session

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,32 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
+import { getServerSession } from 'next-auth'
+
+import prisma from '@/app/libs/prismadb'
+
+// getSession() returns a Promise that resolves to a session object.
+// The session object contains an idToken, which is used to access resources on the server.
+export async function getSession() {
+  const session = await getServerSession(authOptions)
+
+  return session
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession()
+
+    if (!session?.user?.email) return null
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string
+      }
+    })
+
+    if (!currentUser) return null
+
+    return currentUser
+  } catch (error) {
+    return null
+  }
+}
