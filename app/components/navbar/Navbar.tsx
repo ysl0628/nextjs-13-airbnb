de08eec2e7d22d6e6c08abc1138c489da0f1a01a
ysl0628 <yihsinlan@gmail.com>
2023-04-08 23:19:29 +0800

    feat(category): create Categories in homepage

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
 import Container from '../Container'
+import Categories from './Categories'
 
 interface NavbarProps {
   currentUser: SafeUser | null
@@ -24,6 +25,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   )
 }
