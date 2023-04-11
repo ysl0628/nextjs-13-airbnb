d85d8d10abe277823dd8b1db1140e1409cb198ab
ysl0628 <yihsinlan@gmail.com>
2023-04-11 08:27:54 +0800

    feat(home): implement card on home page

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import Heading from './Heading'
+import Button from './Button'
+
+interface EmptyStateProps {
+  title?: string
+  subtitle?: string
+  showReset?: boolean
+}
+
+const EmptyState: React.FC<EmptyStateProps> = ({
+  title = 'No exact matches found.',
+  subtitle = 'Try changing your search or filter to find more results.',
+  showReset
+}) => {
+  const router = useRouter()
+  return (
+    <div
+      className="
+        h-[60vh]
+        flex 
+        flex-col 
+        gap-2 
+        justify-center 
+        items-center 
+      "
+    >
+      <Heading title={title} subtitle={subtitle} center />
+      <div className="w-48 mt-4">
+        {showReset && (
+          <Button
+            label="Remove all filters"
+            onClick={() => router.push('/')}
+            outline
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default EmptyState
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,55 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+import getListings from './actions/getListings'
+import getCurrentUser from './actions/getCurrentUser'
+
+import Container from './components/Container'
+import ClientOnly from './components/ClientOnly'
+import EmptyState from './components/EmptyState'
+import ListingCard from './components/listings/ListingCard'
+
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello Airbnb!</div>
+export default async function Home() {
+  const listings = await getListings()
+  const currentUser = await getCurrentUser()
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
+
+  console.log(listings)
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  )
 }
