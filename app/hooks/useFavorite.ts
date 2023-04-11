0388af70a0f1946a01b0473534c2216005caa8c2
ysl0628 <yihsinlan@gmail.com>
2023-04-11 09:20:16 +0800

    feat(favorite): create status and toggle feature hook

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,56 @@
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import { useCallback, useMemo, MouseEvent } from 'react'
+import { toast } from 'react-hot-toast'
+
+import { SafeUser } from '../types'
+
+import useLoginModal from './useLoginModal'
+
+interface IFavorite {
+  currentUser?: SafeUser | null
+  listingId: string
+}
+
+const useFavorite = ({ currentUser, listingId }: IFavorite) => {
+  const router = useRouter()
+  const loginModal = useLoginModal()
+
+  const hasFavorite = useMemo(() => {
+    return currentUser?.favoriteIds?.includes(listingId)
+  }, [currentUser, listingId])
+
+  const toggleFavorite = useCallback(
+    async (e: MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation()
+
+      if (!currentUser) {
+        loginModal.onOpen()
+        return
+      }
+
+      try {
+        let request
+        if (hasFavorite) {
+          request = () => axios.delete(`/api/favorites/${listingId}`)
+        } else {
+          request = () => axios.post(`/api/favorites/${listingId}`)
+        }
+
+        await request()
+        router.refresh()
+        toast.success('成功')
+      } catch (error) {
+        toast.error('Something went wrong')
+      }
+    },
+    [currentUser, hasFavorite, listingId, loginModal, router]
+  )
+
+  return {
+    hasFavorite,
+    toggleFavorite
+  }
+}
+
+export default useFavorite
