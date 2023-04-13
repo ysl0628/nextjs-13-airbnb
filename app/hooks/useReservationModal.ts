c15d30eae8a3a5e940fda4459e80fc781aaccdbe
ysl0628 <yihsinlan@gmail.com>
2023-04-13 11:27:34 +0800

    feat(reservation): create useReservationModal by zustand

---
+++ app/hooks/useReservationModal.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+
+interface ReservationModalStore {
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
+}
+
+const useReservationModal = create<ReservationModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false })
+}))
+
+export default useReservationModal
